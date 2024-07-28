b6dcf65a6b5a8d9e6862245e82c87def0e456847
lchyeong@gmail.com <lchyeong@gmail.com>
2024-07-28 16:18:19 +0900

    chore: DB 세팅 및 yml공통 설정 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,14 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	implementation("org.apache.httpcomponents:httpclient:4.5.13")
+
+	implementation("org.springframework.boot:spring-boot-starter-actuator") // 상태 모니터링 및 관리 기능
+	implementation("org.springframework.boot:spring-boot-starter-validation") // MVC 유효성 검증 기능
+	implementation("com.fasterxml.jackson.core:jackson-databind") // JSON 데이터 바인딩
+	implementation("org.springdoc:springdoc-openapi-ui:1.7.0") // OpenAPI 3.0 문서 생성
+
 }
 
 tasks.withType<Test> {
