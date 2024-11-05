089bf5f989312b83a9c7f97bfecce2a5de1e2593
lchyeong <lchyeong@gmail.com>
2024-11-05 18:41:38 +0900

    refactor(CookieUtil): 쿠키 세팅을 하는 로직을 분리

---
+++ src/main/java/com/bbansrun/project1/domain/auth/util/CookieUtil.java
@@ -0,0 +1,56 @@
+package com.bbansrun.project1.domain.auth.util;
+
+import com.bbansrun.project1.global.jwt.JwtProperties;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CookieUtil {
+
+    //yml에서 관리하면 장단점?
+    @Value("${secure.cookie}")
+    private boolean secureFlag;
+
+    private final JwtProperties jwtProperties;
+
+    //쿠키 옵션 종류는 뭐있지?
+    public void addRefreshTokenCookie(HttpServletResponse response, String refreshToken) {
+        ResponseCookie refreshCookie = ResponseCookie.from("refreshToken", refreshToken)
+                .httpOnly(true)
+                .secure(secureFlag)
+                .path("/")
+                .maxAge(jwtProperties.getRefreshExpiration() / 1000)
+                .sameSite("Strict")
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
+    }
+
+    public void expireRefreshTokenCookie(HttpServletResponse response) {
+        ResponseCookie responseCookie = ResponseCookie.from("refreshToken", "")
+                .httpOnly(true)
+                .secure(secureFlag)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, responseCookie.toString());
+    }
+
+    public static String getRefreshTokenFromCookie(HttpServletRequest request) {
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if ("refreshToken".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+}
