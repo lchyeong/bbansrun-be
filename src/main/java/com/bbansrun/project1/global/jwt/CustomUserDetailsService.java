f0cd5928ec336866df31e1f1957b5c369ee3385e
lchyeong <lchyeong@gmail.com>
2024-11-13 20:36:55 +0900

    refactor: 불필요한 주석 제거

--- src/main/java/com/bbansrun/project1/global/jwt/CustomUserDetailsService.java
+++ src/main/java/com/bbansrun/project1/global/jwt/CustomUserDetailsService.java
@@ -18,23 +18,17 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    /**
-     * 사용자의 이메일을 받아서 사용자 정보를 조회하고, UserDetails로 변환하여 반환 최초 로그인시 사용
-     */
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email)
-            .orElseThrow(() -> new ApiException(ErrorCode.USER_NOT_FOUND));
+                .orElseThrow(() -> new ApiException(ErrorCode.USER_NOT_FOUND));
 
         return new CustomUserDetails(user);
     }
 
-    /**
-     * 사용자의 UUID를 받아서 사용자 정보를 조회하고, UserDetails로 변환하여 반환 JWT 토큰을 검증할 때 사용
-     */
     public UserDetails loadUserByUuid(UUID userUuid) {
         User user = userRepository.findByUserUuid(userUuid)
-            .orElseThrow(() -> new ApiException(ErrorCode.USER_NOT_FOUND));
+                .orElseThrow(() -> new ApiException(ErrorCode.USER_NOT_FOUND));
 
         return new CustomUserDetails(user);
     }
