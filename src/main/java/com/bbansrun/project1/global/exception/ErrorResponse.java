6e021cb6f28af9a43fb0820cbb186365c02984a5
lchyeong <lchyeong@gmail.com>
2024-11-18 21:17:00 +0900

    refactor(ErrorResponse): 불필요한 주석 제거

--- src/main/java/com/bbansrun/project1/global/exception/ErrorResponse.java
+++ src/main/java/com/bbansrun/project1/global/exception/ErrorResponse.java
@@ -5,9 +5,8 @@ import org.springframework.http.HttpStatus;
 
 public record ErrorResponse(HttpStatus status, int statusCode, String code) {
 
-    // ErrorCode를 받아 ErrorResponse를 생성하는 record 메서드
     public static ErrorResponse of(ErrorCode errorCode) {
         return new ErrorResponse(errorCode.getStatus(), errorCode.getStatusCode(),
-            errorCode.getCode());
+                errorCode.getCode());
     }
 }
